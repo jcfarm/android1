350a8883d7bdaa3b8a9902d0709768680e703fe5
hello-brothers <lh1479784191@qq.com>
2017-05-18 23:02:08 +0800

    home_okhttp_1
    主页农田列表网络连接

--- app/src/main/java/scujcc/com/farm_demo/adapter/HomeRecyclerViewAdapter.java
+++ app/src/main/java/scujcc/com/farm_demo/adapter/HomeRecyclerViewAdapter.java
@@ -9,11 +9,14 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import java.util.List;
+
 import scujcc.com.farm_demo.R;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import scujcc.com.farm_demo.bean.FiledsBean;
 import scujcc.com.farm_demo.viewPager.home.HomeDetails;
 import scujcc.com.farm_demo.viewPager.monitor.MonitorDetails;
 
@@ -25,11 +28,13 @@ public class HomeRecyclerViewAdapter extends RecyclerView.Adapter<HomeRecyclerVi
     private final LayoutInflater mLayoutInflater;
     private final Context mContext;
     private String[] mTitles;
+    private final List<FiledsBean.ItemFieldsBean> mdatas;
 
-    public HomeRecyclerViewAdapter(Context context) {
+    public HomeRecyclerViewAdapter(Context context, List<FiledsBean.ItemFieldsBean> datas) {
         mTitles = context.getResources().getStringArray(R.array.titles);
         mContext = context;
         mLayoutInflater = LayoutInflater.from(context);
+        mdatas = datas;
     }
 
     @Override
@@ -39,14 +44,14 @@ public class HomeRecyclerViewAdapter extends RecyclerView.Adapter<HomeRecyclerVi
 
     @Override
     public void onBindViewHolder(HomeTextViewHolder holder, int position) {
-        holder.mTextView_home_rough.setText("编号："+(position+1)+"号"+"\n"+"温度："+mTitles[position]+"\n"+"湿度："+mTitles[position]);
+        holder.mTextView_home_rough.setText("编号："+mdatas.get(position).getId()+"号"+"\n"+"\n"+"温度："+mdatas.get(position).getTemp()+"摄氏度");
         holder.mTextView_id.setText((position+1)+"号田");
         holder.mTextView_time.setText((position+1)+"分钟前操作");
     }
 
     @Override
     public int getItemCount() {
-        return mTitles == null ? 0 : mTitles.length;
+        return mTitles == null ? 0 : mdatas.size();
     }
 
     public class HomeTextViewHolder extends RecyclerView.ViewHolder {
--- app/src/main/java/scujcc/com/farm_demo/viewPager/HomeFragment.java
+++ app/src/main/java/scujcc/com/farm_demo/viewPager/HomeFragment.java
@@ -11,19 +11,37 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.OrientationHelper;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.zhy.http.okhttp.OkHttpUtils;
+import com.zhy.http.okhttp.callback.StringCallback;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import okhttp3.Call;
+import okhttp3.Request;
 import scujcc.com.farm_demo.R;
 import scujcc.com.farm_demo.adapter.HomeRecyclerViewAdapter;
 import scujcc.com.farm_demo.adapter.HomeRecyclerViewAdapter;
 import scujcc.com.farm_demo.base.BaseFragment;
+import scujcc.com.farm_demo.bean.FiledsBean;
+import scujcc.com.farm_demo.uitls.CacheUtils;
+
+import static com.zhy.http.okhttp.log.LoggerInterceptor.TAG;
 
 /**
  * Created by hello-brothers on 2017/5/3.
@@ -37,11 +55,20 @@ public class HomeFragment extends BaseFragment {
     public static final int TYPE_STAGGERED_GRID_LAYOUT = 3;
     @Bind(R.id.recycler_view)
     RecyclerView mRecyclerView;
+    @Bind(R.id.progressBar)
+    ProgressBar progressBar;
+    @Bind(R.id.tv_nodata)
+    TextView tv_nodata;
 
     private int type = TYPE_LINEAR_LAYOUT;
 
     private ArrayList<BaseFragment> mBaseFragment;
     private TabLayout tabLayout;
+    /**
+     * 网络链接
+     */
+    private String url;
+    private static List<FiledsBean.ItemFieldsBean> datas;
 
     public static HomeFragment newInstance(int type) {
 
@@ -67,6 +94,7 @@ public class HomeFragment extends BaseFragment {
         TextView textView = (TextView) view.findViewById(R.id.home_textView);
         textView.setText(R.string.home);
         ButterKnife.bind(this, view);
+        getDataFromNet();
 
         return view;
     }
@@ -86,13 +114,204 @@ public class HomeFragment extends BaseFragment {
         } else {
             mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));//这里用线性显示 类似于list view
         }
-        mRecyclerView.setAdapter(new HomeRecyclerViewAdapter(getActivity()));
-        mRecyclerView.setItemAnimator(new DefaultItemAnimator());
+        if(datas != null && datas.size() >0){
+            //有数据
+            tv_nodata.setVisibility(View.GONE);
+            //显示适配器
+            Log.e(TAG,"datas"+datas);
+            mRecyclerView.setAdapter(new HomeRecyclerViewAdapter(getActivity(),datas));
+            mRecyclerView.setItemAnimator(new DefaultItemAnimator());
+        }else{
+            //没有数据
+            tv_nodata.setVisibility(View.VISIBLE);
+        }
     }
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
         ButterKnife.unbind(this);
+    }
+
+    /**
+     * -------------------------------------------
+     *                  网络请求
+     */
+
+    private void getDataFromNet() {
+        Log.e(TAG,"getDataFromNet=============");
+        url = "http://118.113.129.208:8090/FileUpload/upload/fields.txt";
+        //得到缓存的数据
+        String saveJson = CacheUtils.getString(getActivity(),url);
+        Log.e(TAG,"=====saveJson====");
+        if(!TextUtils.isEmpty(saveJson)){
+            String json = "\"fields\": [\n" +
+                    "                                                                            {\n" +
+                    "                                                                                \"id\": 1,\n" +
+                    "                                                                                \"url\": \"resp: //12343412\",\n" +
+                    "                                                                                \"temp\": 20\n" +
+                    "                                                                            },\n" +
+                    "                                                                            {\n" +
+                    "                                                                                \"id\": 2,\n" +
+                    "                                                                                \"url\": \"resp: //1232134\",\n" +
+                    "                                                                                \"temp\": 20\n" +
+                    "                                                                            }\n" +
+                    "                                                                        ]\n" +
+                    "                                                                    }";
+            processData(saveJson);
+            Log.e(TAG,"savejson"+json);
+        }
+
+
+        OkHttpUtils
+                .post()
+                .url(url)
+                .id(100)
+                .build()
+                .execute(new MyStringCallback());
+    }
+
+    public class MyStringCallback extends StringCallback {
+
+        @Override
+        public void onBefore(Request request, int id) {
+
+            Toast.makeText(getActivity(), "loading", Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onAfter(int id) {
+
+            Toast.makeText(getActivity(), "finish", Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onError(Call call, Exception e, int id) {
+            e.printStackTrace();
+//            tv_nodata.setVisibility(View.VISIBLE);
+        }
+
+        @Override
+        public void onResponse(String response, int id) {
+            Log.e(TAG,"onResponse=======");
+            Log.e(TAG, "onResponse：complete");
+            String json = "\"fields\": [\n" +
+                    "                                                                            {\n" +
+                    "                                                                                \"id\": 1,\n" +
+                    "                                                                                \"url\": \"resp: //12343412\",\n" +
+                    "                                                                                \"temp\": 20\n" +
+                    "                                                                            },\n" +
+                    "                                                                            {\n" +
+                    "                                                                                \"id\": 2,\n" +
+                    "                                                                                \"url\": \"resp: //1232134\",\n" +
+                    "                                                                                \"temp\": 20\n" +
+                    "                                                                            }\n" +
+                    "                                                                        ]\n" +
+                    "                                                                    }";
+            tv_nodata.setVisibility(View.GONE);
+
+            switch (id) {
+                case 100:
+                    Toast.makeText(getActivity(), "http", Toast.LENGTH_SHORT).show();
+                    break;
+                case 101:
+                    Toast.makeText(getActivity(), "https", Toast.LENGTH_SHORT).show();
+                    break;
+            }
+            //解析数据和显示数据
+            if(response != null){
+                //缓存数据
+                CacheUtils.putString(getActivity(),url,response);
+                processData(response);
+                Log.e(TAG,"respomse"+response);
+
+            }
+        }
+
+
+
+        @Override
+        public void inProgress(float progress, long total, int id) {
+            Log.e(TAG, "inProgress:" + progress);
+        }
+    }
+
+
+
+    /**
+     * 解析和显示数据
+     * @param json
+     */
+    private void processData(String json) {
+
+        //解析数据
+        FiledsBean FiledsBean = parsedJson(json);
+        datas =  FiledsBean.getFields();
+
+//        if(datas != null && datas.size() >0){
+//            //有数据
+////            tv_nodata.setVisibility(.GONE);
+//            //显示适配器
+////            adapter = new OKHttpListAdapter(OKHttpListActivity.this,datas);
+////            listView.setAdapter(adapter);
+//            Log.e(TAG,"datas"+datas);
+////            mRecyclerView.setAdapter(new HomeRecyclerViewAdapter(getActivity(),datas));
+////            mRecyclerView.setItemAnimator(new DefaultItemAnimator());
+//        }else{
+//            //没有数据
+////            tv_nodata.setVisibility(View.VISIBLE);
+//        }
+
+        progressBar.setVisibility(View.GONE);
+    }
+
+
+    /**
+     * 解析json数据
+     *
+     * @param response
+     * @return
+     */
+    private FiledsBean parsedJson(String response) {
+        FiledsBean FiledsBean = new FiledsBean();
+
+        try {
+            JSONObject jsonObject = new JSONObject(response);
+            JSONArray jsonArray = jsonObject.optJSONArray("fields");
+            if (jsonArray != null && jsonArray.length() > 0) {
+                List<FiledsBean.ItemFieldsBean> fields = new ArrayList<>();
+                FiledsBean.setFields(fields);
+                Log.e(TAG,"=======jsonArray.length()"+jsonArray.length());
+                for (int i = 0; i < jsonArray.length(); i++) {
+
+                    JSONObject jsonObjectItem = (JSONObject) jsonArray.get(i);
+
+                    if (jsonObjectItem != null) {
+
+                        FiledsBean.ItemFieldsBean FiledItem = new FiledsBean.ItemFieldsBean();
+
+                        int id = jsonObjectItem.optInt("id");//name
+                        FiledItem.setId(id);
+
+                        String url = jsonObjectItem.optString("url");//desc
+                        FiledItem.setUrl(url);
+
+                        int temp = jsonObjectItem.optInt("temp");//imageUrl
+                        FiledItem.setTemp(temp);
+
+
+
+                        //把数据添加到集合
+                        fields.add(FiledItem);
+                    }
+                }
+            }
+
+
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return FiledsBean;
     }
 }
